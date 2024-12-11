644b3892154335a6024bb2dc74ab66003ba5bb71
Shubham Singh <shubhamsingh9827630326@gmail.com>
2024-12-11 10:09:11 +0530

    Add files via upload

---
+++ employee management system/src/employee/management/system/JDateChooser.java
@@ -0,0 +1,17 @@
+package employee.management.system;
+
+import java.awt.Color;
+
+public class JDateChooser {
+
+    public void setBounds(int i, int j, int k, int l) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setBounds'");
+    }
+
+    public void setBackground(Color color) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setBackground'");
+    }
+
+}
